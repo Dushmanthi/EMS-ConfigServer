87298cdf1a840c2598a66918ad7aa192c08331f3
dushi <madushanimf@gmail.com>
2023-05-25 15:58:49 +0530

    added ems config server

---
+++ src/test/java/com/dushi/emsconfigserver/EmsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dushi.emsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
